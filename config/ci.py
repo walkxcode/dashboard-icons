6e657335a757fdb1588f807d9c58a6304b16aeef
Jag_k <30597878+jag-k@users.noreply.github.com>
2023-07-31 23:46:00 +0000

    Added table of icons in ICONS.md

--- config/ci.py
+++ config/ci.py
@@ -1,34 +1,45 @@
 from pathlib import Path
 
-config = Path(__file__).parent.resolve()
-root = config.parent
-template_path = config / "TEMPLATE.md"
-icons = root / "ICONS.md"
+CDN_URL = "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/png/"
+ICONS_START = "<!-- ICONS -->"
+ICONS_END = "<!-- END ICONS -->"
+TABLE_HEAD: str = (
+  "\n\n"
+  "| Icon filename | Preview |\n"
+  "| ------------- | ------- |\n"
+)
+
+ROOT = Path(__file__).parent.parent.resolve()
+PNG_FOLDER = ROOT / "png"
+ICONS = ROOT / "ICONS.md"
 
 
 def generate_img_tag(file: Path) -> str:
-    return f'<a href="https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/png/{file.name}"><img src="https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/png/{file.name}" alt="{file.stem}" height="50"></a>'
+    return f'<a href="{CDN_URL}{file.name}"><img src="{CDN_URL}{file.name}" alt="{file.stem}" height="50"></a>'
 
 
-img_tags = [generate_img_tag(x) for x in sorted((root / "png").glob("*.png"))]
-line_number = 0
+def generate_table_row(file: Path) -> str:
+    return f"| `{file.name}` | {generate_img_tag(file)} |"
+
+TABLE_ROWS = "\n".join(generate_table_row(x) for x in sorted(PNG_FOLDER.glob("*.png")))
 
 # Read the template file
-with template_path.open("r", encoding="UTF-8") as f:
-    lines = f.readlines()
+with ICONS.open("r", encoding="UTF-8") as f:
+    data = f.read()
 
-# Find the line that starts with "<!-- ICONS -->"
-for line in lines:
-    if line.startswith("<!-- ICONS -->"):
-        line_number = lines.index(line)
-        break
-
-# Insert the icons after the line
-lines.insert(line_number + 1, " ".join(img_tags))
+# split template by ICONS and END ICONS
+prefix, data = data.split(ICONS_START, 1)
+_, suffix = data.split(ICONS_END, 1)
 
 # Write the new file
-with icons.open("w", encoding="UTF-8") as f:
-    f.write("".join(lines))
+with ICONS.open("w", encoding="UTF-8") as f:
+    f.write(prefix)
+    f.write(ICONS_START)
+    f.write(TABLE_HEAD)
+    f.write(TABLE_ROWS)
+    f.write("\n\n")
+    f.write(ICONS_END)
+    f.write(suffix)
     f.write("\n")
 
 print("Done!")
